ac1541f046dca69db3830271f84a7cd4e4118298
muhammad.raheel <muhammad.raheel@pitb.gov.pk>
2023-06-22 12:08:22 +0500

    Update escape chars

--- src/components/product/cart/CartHoverCard.tsx
+++ src/components/product/cart/CartHoverCard.tsx
@@ -25,31 +25,24 @@ const CartHoverCard:React.FC<Props> = ({ products, amount }) =>{
                             <span className="text-red-500 text-xs">{ item.type }</span>
                         </div>
                         <div className="text-sm font-medium">${ item.totalPrice }</div>
-
                     </div>
-                   
-                    
                 </div> 
             )) }
 
             { (Number(products.length) > 0)? (
-                <div className="flex justify-between">
-                    <Link href={'/cart'}>
-                        <Button className="tracking-widest">Checkout</Button>
-                    </Link>
-                    <h3 className="font-semibold leading-10">${amount.toLocaleString(undefined, { minimumFractionDigits: 2})}</h3>
-                </div>
-            ) : (
+                    <div className="flex justify-between">
+                        <Link href={'/cart'}>
+                            <Button className="tracking-widest">Checkout</Button>
+                        </Link>
+                        <h3 className="font-semibold leading-10">${amount.toLocaleString(undefined, { minimumFractionDigits: 2})}</h3>
+                    </div>
+                 ) : (
                 <div className="flex flex-col gap-4 justify-between">
-                <p>Looks like you haven't added any items yet.</p>
-                <Link href={'/products'} className="flex text-blue-400 hover:underline-offset-4"><GoArrowLeft className="mt-1" />Continue Shopping</Link>
-
-            </div>
-            )
-           
+                    <p>Looks like you haven&apos;t added any items yet.</p>
+                    <Link href={'/products'} className="flex text-blue-400 hover:underline-offset-4"><GoArrowLeft className="mt-1" />Continue Shopping</Link>
+                </div>
+                )
             }
-            
-            
         </>
     )
 }
